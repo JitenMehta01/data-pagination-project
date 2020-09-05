dbcbad732e3fa97e68c0640b71683fe4b749ee3f
Jiten Mehta <jitenmehta03@gmail.com>
2020-09-05 18:34:13 +0100

    completed the showpage function. Read script.js to understand what this function does.

--- js/script.js
+++ js/script.js
@@ -1,24 +1,81 @@
-/*
-Treehouse Techdegree:
-FSJS Project 2 - Data Pagination and Filtering
+// variables
+
+const itemsPerpage = 9; // how many students on each page
+
+const listContainer = document.querySelector('.student-list'); // ul containing list items
+
+
+/*************************************************************************************************************************************************************
+ * 
+ * 
+ * 
+showPage function
+
+- This section contains 2 functions.
+
+- The first function is called Showpage. This function accepts 2 arguements. a array object and a number.
+- The array contains 42 objects.
+- The number represents what page the user will be on.
+
+- First, the function has 2 variables. Startindex and endIndex
+- These variables represents a window within the array object.
+- When called uped, this window will be displayed to the DOM
+
+
+- The listContainer variable (which contains each item) is set to an empty string to make sure nothing else is on the page.
+
+- The array object is then looped upon. A for loop is used.
+
+- In the loop there will be a conditional statement
+- The condiditon will evaluate if the value of i is more or equal, more and less then the indexes stated in teh variabled (i.e the window)
+
+- If true, the studentBuild function is called up and stored in a varaible called student.
+- The studentBuild function will accept 1 arguement. a object from the list parameter.
+- The i variable from the for loop will be used as a index value to indicate which item we want to pass as an arguement.
+- The studentBuild function will then return a html snippet which needs to appended to the listContainer variable.
+
+- We then use the insertAdjaceentHTML method to add each list item to the listContainer method.
+*
 */
 
 
+function showPage (list, page){
+  const startIndex = (page * itemsPerpage) - itemsPerpage;
+  const endIndex = page * itemsPerpage;
 
-/*
-For assistance:
-   Check out the "Project Resources" section of the Instructions tab: https://teamtreehouse.com/projects/data-pagination-and-filtering#instructions
-   Reach out in your Slack community: https://treehouse-fsjs-102.slack.com/app_redirect?channel=unit-2
-*/
+  listContainer.innerHTML = '';
+
+  for(let i =0; i < list.length; i++){
+    if(i >= startIndex && i < endIndex){
+      let student = studentBuild(list[i]); // calls the student function and passes a list item as arguement.
+      listContainer.insertAdjacentHTML('beforeend', student);
+    } 
+  }
+}
 
 
 
-/*
-Create the `showPage` function
-This function will create and insert/append the elements needed to display a "page" of nine students
-*/
 
+function studentBuild (student){ // a function containing a template literal. This is how each student will be appended to the DOM.
+  
+ 
+  let studentItem = `
+  <li class="student-item cf">
+    <div class="student-details">
+      <img class = 'avatar' src = ${student.picture.large}>
+      <h3>${student.name.first} ${student.name.last} </h3>
+      <span class="email">${student.email}</span>
+    </div>
+    <div class="joined-details">
+      <span class="date">Joined ${student.registered.date}</span>
+    </div>
+  </li>
+  `;
 
+  return studentItem;
+}
+
+/*********************************************************************************************************************************************************************************** */
 
 /*
 Create the `addPagination` function
